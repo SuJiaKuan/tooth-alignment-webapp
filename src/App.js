68914237fe69890457a451fe82c4c19acdf17920
SuJiaKuan <feabries@gmail.com>
2022-09-17 01:35:50 +0800

    Add a basic toolbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useCallback, useState } from 'react';
 
 import { useDropzone } from 'react-dropzone';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 import MovementsTable from './MovementsTable'
 import Viewer3D from './Viewer3D';
@@ -30,6 +35,33 @@ function App() {
 
   return (
     <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                flexGrow: 1,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              AGAI x 創兆
+            </Typography>
+            <Button variant="contained" disabled>RUN AI</Button>
+            <Button variant="text" disabled></Button>
+            <Button variant="contained" disabled>SHOW MOVEMENTS</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <header className="App-header">
         <div {...getRootProps()}>
           <input {...getInputProps()} />
